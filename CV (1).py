67493bca08ee03d9e63f7251f0f7651ccadb7cc0
mrsdiouf660-svg <mrsdiouf660@gmail.com>
2026-03-05 08:45:50 +0000

    Add files via upload

---
+++ CV (1).py
@@ -0,0 +1,19 @@
+import streamlit as st 
+with st.sidebar:
+    st.write("SEYNABOU SENE")
+    st.write("CONTACTS")
+    st.write("Mrsdiouf660@gmail.com")
+    st.write("Adresse:DakarPlateau")
+st.write("FORMATIONS")
+st.write("BTS EN GEOMATIQUE")
+st.subheader("PROJETS ACADEMIQUES")
+st.write("Collecte de données")
+st.write("Création de base de données et Géoréférencement")
+st.write("Projet de collecte des Centre de Formations Professionnelles et Techniques")
+st.subheader("EXPERIENCES")
+st.write("Secrétaire Générale de la liste VERE(FLSH/UCAD)")
+st.subheader("COMPETENCES")
+st.write("géoréferencement")
+st.write("numerisation")
+st.write("maitrise des logiciels SIG:ARCGIS et QGIS")
+
